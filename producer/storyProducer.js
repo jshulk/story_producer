5462205c418dd2fc755825baffd7b62997b88c24
vjunloc <vijaytyagi.vt@gmail.com>
2015-08-03 23:16:07 +0530

    publishing working fine

--- producer/storyProducer.js
+++ producer/storyProducer.js
@@ -4,12 +4,6 @@ var config = require("../config/messagingConfig"),
 exports.produce = function(message){
 	var channel = producerChannel.get();
 
-	channel.publish(config.STORY_EXCHANGE, config.ROUTING_KEY, new Buffer(message))
-	.then(function(){
-		console.log("published successfully");
-	})
-	.catch(function(err){
-		console.log("failed to publish");
-		console.log(err);
-	})
+	channel.publish(config.STORY_EXCHANGE, config.ROUTING_KEY, new Buffer(JSON.stringify(message)));
+	
 }
